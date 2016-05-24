2d22eb3059090e51b983920282e354f8f87fe0ff
Juan Carlos Saez Alcaide <jcsaezal@ucm.es>
2016-05-24 23:45:21 +0200

    Typo in script

--- run_test.bat
+++ run_test.bat
@@ -2,5 +2,5 @@ set INPUT_FILE=example.xls
 set NR_FIELD_VALUES=15
 set MASS=0.05
 set OUTPUT_FILE=
-python delta_s %INPUT_FILE% %NR_FIELD_VALUES% %MASS% %OUTPUT_FILE%
+python delta_s.py %INPUT_FILE% %NR_FIELD_VALUES% %MASS% %OUTPUT_FILE%
 pause
